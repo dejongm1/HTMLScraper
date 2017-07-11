4bbb59c90a5c58bd0b064c83c85bf2fb68a4a4e2
U569220 <U569220@DTCF182E08A7702.ent.wfb.bank.corp>
2017-07-11 11:03:41 -0500

    Adding console interaction

--- Scraper/src/com/mcd/scraper/Scraper.java
+++ Scraper/src/com/mcd/scraper/Scraper.java
@@ -1,5 +1,6 @@
 package com.mcd.scraper;
 
+import java.io.Console;
 import java.io.File;
 import java.io.IOException;
 
@@ -8,15 +9,54 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+/**
+ * 
+ * @author U569220
+ *
+ */
 public class Scraper {
 
 	public static void  main(String[] args) throws IOException {
-		/**
-		 * args[0] - url
-		 * args[1] - tags to delimit by
-		 * args[2] - at work (true/false)  **/
+		
+		Console console = System.console();
+		if (console == null) {
+            System.err.println("No console.");
+            System.exit(1);
+        }
+		ScrapingEngine engine = new ScrapingEngine();
+		
+		String scrapingType = console.readLine("What do you want to do? Get popular words or scrape for text?");
+		if (scrapingType.toLowerCase().contains("popular")) {
+			String url = console.readLine("URL: ");
+			int numberOfWords = 0;
+			try {
+				numberOfWords = Integer.valueOf(console.readLine("Number of words: "));
+			} catch (Exception e) {
+				try {
+					numberOfWords = Integer.valueOf(console.readLine("Please enter a enter between 1 and 100: "));
+				} catch (Exception e2) {
+					console.writer().println("Since you don't want to listen, I'll give you 5 words");
+					numberOfWords = 5;
+				}
+			}
+			engine.getPopularWords(url, numberOfWords);
+		} else if (scrapingType.toLowerCase().contains("text")) {
+			String url = console.readLine("URL: ");
+			String selector = console.readLine("selector: ");
+			
+			engine.getTextBySelector(url, selector);
+		}
+		
+	}
+	
+	public static void  mainSingle(String[] args) throws IOException {
+		/** args[0] - method to call
+		 *  
+		 *  args[1] - tags to delimit by
+		 *  args[2] - at work (true/false)  **/
 
 		if (args.length == 0) {
+			System.out.println("What do you want to do? Get popular words or scrape for text?\n use ");
 			System.out.println("You must pass some arguments, separated by spaces, for scraping: ");
 			System.out.println("	* First argument is the URL of the page to be parsed.");
 			System.out.println("	* Second argument is the html tag(s) to get the text of.");
@@ -34,8 +74,8 @@ public class Scraper {
 			String htmlLocation = "";
 			try {
 				Document doc;
-				if (Boolean.valueOf(args[2])) {
-					switch (args[0]) {
+				if (args.length>=4 && Boolean.valueOf(args[3])) {
+					switch (args[1]) {
 						case "https://www.intoxalock.com/" : 									htmlLocation = "intoxalock-homepage.html";
 																								break;
 						case "https://en.wikipedia.org/" : 										htmlLocation = "wikipedia-homepage.html";
@@ -46,12 +86,12 @@ public class Scraper {
 																								break;
 					}
 					File input = new File("htmls/" + htmlLocation);
-					doc = Jsoup.parse(input, "UTF-8", args[0]);
+					doc = Jsoup.parse(input, "UTF-8", args[1]);
 	
 				} else {
-					doc = Jsoup.connect(args[0]).userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36").get();
+					doc = Jsoup.connect(args[1]).userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36").get();
 				}
-				Elements tags = doc.select(args[1]);
+				Elements tags = doc.select(args[2]);
 				for (Element tag : tags) {
 					System.out.println(tag.text());
 				}
---
+++ Scraper/src/com/mcd/scraper/ScrapingEngine.java
@@ -0,0 +1,19 @@
+package com.mcd.scraper;
+/**
+ * 
+ * @author U569220
+ *
+ */
+public class ScrapingEngine {
+
+	
+	
+	protected void getPopularWords(String url, int numberOfWords) {
+		
+	}
+	
+	protected void getTextBySelector(String url, String selector) {
+		
+	}
+	
+}
