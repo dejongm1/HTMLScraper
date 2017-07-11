caacbb099d616628d29ba6e7627ebcc41809d7f1
dejongm1 <michael.dejong@meredith.com>
2017-07-11 07:45:45 -0500

    Initial Commit

---
+++ Scraper/src/com/mcd/scraper/Scraper.java
@@ -0,0 +1,66 @@
+package com.mcd.scraper;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class Scraper {
+
+	public static void  main(String[] args) throws IOException {
+		/**
+		 * args[0] - url
+		 * args[1] - tags to delimit by
+		 * args[2] - at work (true/false)  **/
+
+		if (args.length == 0) {
+			System.out.println("You must pass some arguments, separated by spaces, for scraping: ");
+			System.out.println("	* First argument is the URL of the page to be parsed.");
+			System.out.println("	* Second argument is the html tag(s) to get the text of.");
+			System.out.println("	* Third argument is \"true\" only if using offline or behind a firewall.");
+			System.out.println("  For example: " );
+			System.out.println("	java -jar Scraper.jar http://en.wikipedia.org/ \"#mp-itn b a\"");
+		} else if (args[0].contains("help")) {
+			System.out.println("Usage: ");
+			System.out.println("	* First argument is the URL of the page to be parsed.");
+			System.out.println("	* Second argument is the html tag(s) to get the text of.");
+			System.out.println("	* Third argument is true only if using offline or behind a firewall.");
+			System.out.println("For example: " );
+			System.out.println("	java -jar Scraper.jar https://www.intoxalock.com/iowa/installation-locations \"#dnn_dnnInstalationCenters_instalationLocationList .info-block\" true");
+		} else {
+			String htmlLocation = "";
+			try {
+				Document doc;
+				if (Boolean.valueOf(args[2])) {
+					switch (args[0]) {
+						case "https://www.intoxalock.com/" : 									htmlLocation = "intoxalock-homepage.html";
+																								break;
+						case "https://en.wikipedia.org/" : 										htmlLocation = "wikipedia-homepage.html";
+																								break;
+						case "https://www.intoxalock.com/iowa/installation-locations" : 		htmlLocation = "intoxalock-iowa-locations.html";
+																								break;
+						default : 																htmlLocation = "";
+																								break;
+					}
+					File input = new File("htmls/" + htmlLocation);
+					doc = Jsoup.parse(input, "UTF-8", args[0]);
+	
+				} else {
+					doc = Jsoup.connect(args[0]).userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36").get();
+				}
+				Elements tags = doc.select(args[1]);
+				for (Element tag : tags) {
+					System.out.println(tag.text());
+				}
+	
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
